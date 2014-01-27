76959a9878704c61d0c48d6d8808a4ae63bf932c
Allen Fair <allen.fair@gmail.com>
2014-01-26 21:20:46 -0500

    Adds EmailAddress:Config.providers

--- lib/email_address/config.rb
+++ lib/email_address/config.rb
@@ -1,4 +1,28 @@
+require 'singleton'
+
 module EmailAddress
   class Config
+
+    class Setup
+      attr_reader :providers
+
+      def initialize
+        @providers = {}
+      end
+
+      def do_block(&block)
+        instance_eval(&block)
+      end
+
+      def add_provider(name, defn={})
+        @providers[name] = defn
+      end
+    end
+
+    def self.setup(&block)
+      @setup ||= Setup.new
+      @setup.do_block(&block) if block_given?
+      @setup
+    end
   end
 end
--- test/email_address/config.rb
+++ test/email_address/config.rb
@@ -1,13 +1,12 @@
 #encoding: utf-8
 require_relative '../test_helper'
-#require 'minitest/autorun'
 
-class TestConfig < MiniTest::Unit::TestCase
-  def test_config
+class TestConfig < MiniTest::Test
+  def test_provider
     EmailAddress::Config.setup do
       add_provider :google, domain_names: %w(gmail.com googlemail.com google.com)
     end
-    assert_equal EmailAddress::Config.get.provider_matching_rules.first[:provider], :google
+    assert_equal true, EmailAddress::Config.setup.providers.has_key?(:google)
   end
   
 end
