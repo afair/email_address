fc2263cb65df35f927e89afd73732b419ad06805
f <f@sutty.nl>
2021-02-24 15:47:22 -0300

    allow passing empty hash

--- lib/email_address/config.rb
+++ lib/email_address/config.rb
@@ -194,10 +194,11 @@ module EmailAddress
     # Customize your own error message text.
     def self.error_messages(hash = {}, locale = "en", *extra)
       hash = extra.first if extra.first.is_a? Hash
+
       @errors[locale] ||= {}
       @errors[locale]["email_address"] ||= {}
 
-      unless hash.empty?
+      unless hash.nil? || hash.empty?
         @errors[locale]["email_address"] = @errors[locale]["email_address"].merge(hash)
       end
 
